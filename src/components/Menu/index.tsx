ea6ae501c7ef9b977993c8b5c8d4a5f49b12d85a
mauriani <maurianim@gmail.com>
2022-08-11 15:21:24 -0300

    adicionando json-server

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import { formatPrice } from "../../util/format";
 import { ContainerMenu } from "./styles";
 
+interface listProducts {
+  id: number;
+  title: string;
+  image: string;
+  price: number;
+  type: [];
+}
+
 export function Menu() {
+  const [products, setProducts] = useState<listProducts[]>([]);
+
+  async function loadProducts() {
+    const response = await api.get("products");
+
+    const dataFormatted = response.data.map((product: listProducts) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    setProducts(dataFormatted);
+  }
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  console.log(products);
   return (
     <ContainerMenu>
       <h1>Nossos cafés</h1>
